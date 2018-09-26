6eaaa9dd9b2d31ffa7898076c58ae3f0b70931b9
github username <github email address>
2018-09-26 13:58:47 +0800

    Generate each controller and helper for new models

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        if auth
+            @user = User.create_with_omniauth(auth)
+            session[:user_id] = @user.id
+            render 'users/show'
+        else
+            @user = User.find_by_email(params[:user][:email])
+            @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            render 'users/show'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_path
+    end
+    
+    private
+    
+    def auth
+        request.env['omniauth.auth']
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        session[:user_id] = @user.id
+        render 'users/show'
+    end
+
+    def show
+        @user = User.find_by(id: session[:user_id])
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
 end
