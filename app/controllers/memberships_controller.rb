3414588f4536f3b1208a2175430c97e0838c0682
github username <github email address>
2018-10-05 14:26:04 +0700

    Add user to club membership

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,10 @@
 class MembershipsController < ApplicationController
+
+    def new
+        @membership = Membership.create(:user_id => params[:user_id], :club_id => params[:club_id])
+        @club << @user
+        # @message = @membership.new_member
+        # redirect_to user_path(@club.user, :message => @message)
+    end
+
 end
