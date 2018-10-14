afdccc52b2b067942cf3bc5b3a5ffff40b891bfa
github username <github email address>
2018-10-14 16:37:19 +0800

    Implement follow and unfollow clubs from memberships page

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,10 +1,46 @@
 class MembershipsController < ApplicationController
 
+    # before_action :set_club
+    before_action :set_user
+
+    def index
+        @clubs = @user.followees(Club)
+    end
+
     def new
-        @membership = Membership.create(:user_id => params[:user_id], :club_id => params[:club_id])
-        @club << @user
-        # @message = @membership.new_member
-        # redirect_to user_path(@club.user, :message => @message)
+        @club = Club.find(params[:club_id])
+        if @user.follows?(@club) == true
+            flash[:notice] = "You have already joined #{@club.name}."
+            redirect_to memberships_path
+        else
+            @membership = Membership.create(:user_id => params[:user_id], :club_id => params[:club_id])
+            @club.users << @user
+            @user.follow!(@club)
+            flash[:success] = "You have successfully joined #{@club.name}."
+            redirect_to memberships_path
+        end
+    end
+
+    def update
+        @club = Club.find(params[:id])
+        @user.follow!(@club)
+        redirect_to club_path(@club)
+    end
+
+    def destroy
+        @club = Club.find(params[:id])
+        @user.unfollow!(@club)
+        redirect_to club_path(@club)
+    end
+
+    private
+
+    def set_club
+        @club = Club.find(params[:club_id])
+    end
+
+    def set_user
+        @user = current_user
     end
 
 end
--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -1,2 +1,21 @@
 module ClubsHelper
+
+    def link_to_toggle_club_follow(club)
+        url = membership_path(club)
+
+        if club.followed_by?(current_user)
+            link_to("Unfollow", url, {
+                method: 'DELETE',
+                remote: true,
+                class: 'follow btn btn-primary',
+            })
+        else
+            link_to("Follow", url, {
+                method: 'PUT',
+                remote: true,
+                class: 'follow btn',
+            })
+        end
+    end
+
 end
