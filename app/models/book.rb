4be927212b993e5af0e93631ee897b30463c9e18
github username <github email address>
2018-10-15 14:33:52 +0800

    Set up Readings association with existing models

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,12 +4,10 @@ class Book < ApplicationRecord
     belongs_to :shelf, optional: true
     delegate :club, to: :shelf, allow_nil: true
 
+    has_many :readings
+    has_many :discussions, through: :readings
+    has_many :comments, through: :discussions
+
     acts_as_likeable
-
-    # has_many :shelves
-    # has_many :clubs, through: :shelves
-
-    # has_many :discussions
-    # has_many :comments, through: :discussions
 
 end
