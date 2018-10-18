9cef9cd95b1036f0de00d6fe59fa6db4f4d77a70
github username <github email address>
2018-10-18 22:37:10 +0800

    Add CRUD for shelf

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,2 +1,20 @@
 class ShelvesController < ApplicationController
+
+    def new
+        @shelf = Shelf.new
+    end
+
+    def show
+        @shelf = Shelf.find(params[:id])
+        @current_book = Book.find(@shelf.current_book) if @shelf.current_book
+        @upcoming_books = @shelf.upcoming_books.map { |book| Book.find(book) }
+        @read_books = @shelf.read_books.map { |book| Book.find(book) }
+    end
+
+    private
+
+    def shelf_params
+        params.require(:shelf).permit(:current_book, :read_books, :upcoming_books)
+    end
+
 end
