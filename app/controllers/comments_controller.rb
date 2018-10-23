570918c1a3b6c7a4470b5651ff5bb81e38badfad
github username <github email address>
2018-10-23 13:55:30 +0800

    Add CRUD for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,35 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        @discussion = Discussion.find(params[:discussion_id])
+        @comment = @discussion.comments.create(comment_params)
+        @comment.user_id = current_user.id
+        
+        if @comment.save
+            flash[:success] = "Your comment was posted."
+            redirect_to club_shelf_book_path(:id => params[:book_id])
+        else
+            flash[:danger] = "Something went wrong and we could not save your comment. Please try again."
+            redirect_to club_shelf_book_path(:id => params[:book_id])
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:comment_id])
+        if @comment.user == current_user
+            @comment.destroy
+            flash[:warning] = "You have deleted your comment."
+        else
+            flash[:danger] = "You do not have permission to delete this comment."
+        end
+        redirect_to club_shelf_book_path(:id => params[:book_id])
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body)
+    end
+
 end
