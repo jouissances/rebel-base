04866489cf2c1f04a09af8d3c77be1ac397f9913
github username <github email address>
2018-10-31 12:57:01 +0800

    Specify `dependent: :destroy`

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -1,6 +1,6 @@
 class Shelf < ApplicationRecord
-    belongs_to :club
-    has_many :books
+    belongs_to :club, :dependent => :destroy
+    has_many :books, :dependent => :destroy
 
     serialize :read_books, Array
     serialize :upcoming_books, Array
